e5dd202bd52f1e1f49fdabce234f4633509a3d81
dickyzuerst <dzuerst@gmail.com>
2024-10-13 17:15:11 +0700

    clean bottom nav

--- app/src/main/java/com/neonusa/tanciku/presentation/navigator/BottomNavigationitem.kt
+++
@@ -1,8 +0,0 @@
-package com.neonusa.tanciku.presentation.navigator
-
-import androidx.annotation.DrawableRes
-
-data class BottomNavigationItem(
-    @DrawableRes val icon: Int,
-    val text: String
-)
