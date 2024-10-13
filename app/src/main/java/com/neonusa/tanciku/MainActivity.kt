7fd212ee2c718637e780493b1492ef9ce69c1d53
dickyzuerst <dzuerst@gmail.com>
2024-10-13 17:29:20 +0700

    fix dark theme

--- app/src/main/java/com/neonusa/tanciku/MainActivity.kt
+++ app/src/main/java/com/neonusa/tanciku/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.neonusa.tanciku.presentation.home.HomeScreen
 import com.neonusa.tanciku.ui.theme.TancikuTheme
 
@@ -19,11 +26,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TancikuTheme {
+            TancikuTheme(dynamicColor = false) {
+                // THEME
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiColor = rememberSystemUiController()
+                SideEffect {
+                    systemUiColor.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+//                    NavGraph(startDestination = viewModel.startDestination.value)
+//                    EmptyScreen()
                 HomeScreen()
+
+                }
             }
         }
-
     }
 }
 
