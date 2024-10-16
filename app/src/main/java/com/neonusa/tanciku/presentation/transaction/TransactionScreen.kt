4462444a35ee5c1894d6ab0a0843689ca34fe35b
dickyzuerst <dzuerst@gmail.com>
2024-10-16 20:13:08 +0700

    add list transaction on transaction screen

--- app/src/main/java/com/neonusa/tanciku/presentation/transaction/TransactionScreen.kt
+++ app/src/main/java/com/neonusa/tanciku/presentation/transaction/TransactionScreen.kt
@@ -1,16 +1,49 @@
 package com.neonusa.tanciku.presentation.transaction
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.neonusa.tanciku.R
+import com.neonusa.tanciku.presentation.Dimens.MediumPadding1
+import com.neonusa.tanciku.presentation.common.ListTransactionitem
+import com.neonusa.tanciku.presentation.home.dummyItems
 
 @Composable
 fun TransactionScreen(){
     Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(top = MediumPadding1, start = MediumPadding1, end = MediumPadding1)
     ) {
-        Text(text = "This is transaction screen")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Transaksi",
+                color = colorResource(id = R.color.text_title_large),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        ListTransactionitem(itemList = dummyItems)
     }
 }
 
