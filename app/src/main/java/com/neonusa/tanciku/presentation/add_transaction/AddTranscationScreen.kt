b1b7b92d55ade21854fe8a0c90403ec2e4c45a0e
dickyzuerst <dzuerst@gmail.com>
2024-10-16 11:29:20 +0700

    add two new input field in add transction

--- app/src/main/java/com/neonusa/tanciku/presentation/add_transaction/AddTranscationScreen.kt
+++ app/src/main/java/com/neonusa/tanciku/presentation/add_transaction/AddTranscationScreen.kt
@@ -2,12 +2,26 @@ package com.neonusa.tanciku.presentation.add_transaction
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.neonusa.tanciku.R
 import com.neonusa.tanciku.presentation.add_transaction.components.AddTransactionToolbarLayout
 import com.neonusa.tanciku.presentation.add_transaction.components.TransactionTypeLayout
 
@@ -15,6 +29,8 @@ import com.neonusa.tanciku.presentation.add_transaction.components.TransactionTy
 fun AddTransactionScreen(
     navigateUp: () -> Unit
 ){
+    var nominal by remember{ mutableStateOf("0")}
+    var keterangan by remember{ mutableStateOf("")}
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -23,6 +39,33 @@ fun AddTransactionScreen(
         // rencananya nanti pilih tanggalnya kalo udh tekan centang aja
         AddTransactionToolbarLayout(onBackClick = navigateUp, onShareClick = {})
         TransactionTypeLayout()
+        OutlinedTextField(
+            value = nominal,
+            leadingIcon = {
+                Text("Rp")
+            },
+            onValueChange = { newText ->
+                nominal = newText
+            },
+            label = {Text("Nominal")},
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 4.dp))
+
+        OutlinedTextField(
+            value = keterangan,
+            onValueChange = { newText ->
+                keterangan = newText
+            },
+            label = { Text(text = "Keterangan")},
+            shape = RoundedCornerShape(16.dp),
+            maxLines =  5,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding( start= 16.dp, end= 16.dp, top = 8.dp)
+                .defaultMinSize(minHeight = 100.dp)
+        )
 
 //        DatePickerLayout()
 
