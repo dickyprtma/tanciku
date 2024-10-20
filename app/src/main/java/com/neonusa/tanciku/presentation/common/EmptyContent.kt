8d656aeebe98825a67ae325794dcb561a48a1ac4
dickyzuerst <dzuerst@gmail.com>
2024-10-21 00:28:30 +0700

    menambahkan empty content

---
+++ app/src/main/java/com/neonusa/tanciku/presentation/common/EmptyContent.kt
@@ -0,0 +1,57 @@
+package com.neonusa.tanciku.presentation.common
+
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.neonusa.tanciku.R
+import com.neonusa.tanciku.ui.theme.TancikuTheme
+
+@Composable
+fun EmptyContent(alphaAnim: Float, message: String, iconId: Int) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = null,
+            tint = if (isSystemInDarkTheme()) LightGray else DarkGray,
+            modifier = Modifier
+                .size(80.dp)
+                .alpha(alphaAnim)
+        )
+        Text(
+            modifier = Modifier
+                .padding(24.dp)
+                .alpha(alphaAnim),
+            text = message,
+            style = MaterialTheme.typography.bodyMedium,
+            color = if (isSystemInDarkTheme()) LightGray else DarkGray,
+        )
+    }
+}
--- app/src/main/java/com/neonusa/tanciku/presentation/common/ListTranscationItem.kt
+++ app/src/main/java/com/neonusa/tanciku/presentation/common/ListTranscationItem.kt
@@ -1,5 +1,7 @@
 package com.neonusa.tanciku.presentation.common
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +13,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -19,6 +26,7 @@ import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.neonusa.tanciku.R
 import com.neonusa.tanciku.domain.model.Transaction
 import com.neonusa.tanciku.domain.model.TransactionCategory
 import com.neonusa.tanciku.domain.model.TransactionType
@@ -27,41 +35,34 @@ import com.neonusa.tanciku.presentation.home.components.TransactionItem
 @Composable
 fun ListTransactionItem(
     transactions: List<Transaction>,
-    onClick:(Transaction) -> Unit) {
-    LazyColumn {
-        items(count = transactions.size) {
-            val transaction = transactions[it]
-            TransactionItem(
-                transaction = transaction, onClick = {onClick(transaction)}
-            )
+    onClick:(Transaction) -> Unit,
+    ) {
+    if (transactions.isEmpty()){
+        var startAnimation by remember {
+            mutableStateOf(false)
+        }
+
+        val alphaAnimation by animateFloatAsState(
+            targetValue = if (startAnimation) 0.3f else 0f,
+            animationSpec = tween(durationMillis = 1000)
+        )
+
+        LaunchedEffect(key1 = true) {
+            startAnimation = true
+        }
+
+        EmptyContent(alphaAnim = alphaAnimation, message = "Belum ada transaksi", iconId = R.drawable.no_transaction)
+    } else {
+        LazyColumn {
+            items(count = transactions.size) {
+                val transaction = transactions[it]
+                TransactionItem(
+                    transaction = transaction, onClick = {onClick(transaction)}
+                )
+            }
         }
     }
-}
 
-@Composable
-fun EmptyContent(alphaAnim: Float, message: String, iconId: Int) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Icon(
-            painter = painterResource(id = iconId),
-            contentDescription = null,
-            tint = if (isSystemInDarkTheme()) LightGray else DarkGray,
-            modifier = Modifier
-                .size(120.dp)
-                .alpha(alphaAnim)
-        )
-        Text(
-            modifier = Modifier
-                .padding(10.dp)
-                .alpha(alphaAnim),
-            text = message,
-            style = MaterialTheme.typography.bodyMedium,
-            color = if (isSystemInDarkTheme()) LightGray else DarkGray,
-        )
-    }
 }
 
 @Preview(showBackground = true)
