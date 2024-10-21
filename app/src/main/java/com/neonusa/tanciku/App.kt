8b3354a09ee7aadef5b824830cf18e929b777f5d
dickyzuerst <dzuerst@gmail.com>
2024-10-21 16:15:21 +0700

    admob banner implemented successfully

--- app/src/main/java/com/neonusa/tanciku/App.kt
+++ app/src/main/java/com/neonusa/tanciku/App.kt
@@ -1,8 +1,12 @@
 package com.neonusa.tanciku
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
 class App: Application(){
@@ -10,5 +14,11 @@ class App: Application(){
         super.onCreate()
         // untuk menggunakan library threetenabp (convert format date)
         AndroidThreeTen.init(this);
+
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
+        backgroundScope.launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(this@App) {}
+        }
     }
 }
---
+++ app/src/main/java/com/neonusa/tanciku/presentation/common/AdmobBannerAd.kt
@@ -0,0 +1,36 @@
+package com.neonusa.tanciku.presentation.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun AdMobBannerAd(
+    modifier: Modifier = Modifier,
+    adUnitId: String = "ca-app-pub-3940256099942544/6300978111", // Test ad unit ID. Replace with your own.
+    adSize: AdSize = AdSize.BANNER
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .then(modifier), // Preserve the custom modifier if provided
+        contentAlignment = Alignment.Center // Center the content horizontally
+    ) {
+        AndroidView(
+            modifier = Modifier,
+            factory = { context ->
+                AdView(context).apply {
+                    setAdSize(adSize)
+                    this.adUnitId = adUnitId
+                    loadAd(AdRequest.Builder().build())
+                }
+            }
+        )
+    }
+}
