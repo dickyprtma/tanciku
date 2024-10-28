e85c96fd6880afe96491ade5a48923845633d992
dickyzuerst <dzuerst@gmail.com>
2024-10-28 22:26:58 +0700

    mengembalikan tanciku+ text

--- app/src/main/java/com/neonusa/tanciku/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/neonusa/tanciku/presentation/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,15 +15,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.neonusa.tanciku.R
@@ -76,19 +80,19 @@ fun HomeScreen(
                     .height(30.dp)
             )
 
-//            Text(
-//                text = "Tanciku+",
-//                color = Color(0xFF94882B), // Warna emas atau warna yang menunjukkan premium
-//                style = MaterialTheme.typography.labelSmall.copy(
-//                    fontWeight = FontWeight.Bold
-//                ),
-//                modifier = Modifier
-//                    .background(
-//                        color = Color(0xFFFCF7D0), // Warna latar untuk badge
-//                        shape = RoundedCornerShape(8.dp)
-//                    )
-//                    .padding(horizontal = 8.dp, vertical = 4.dp)
-//            )
+            Text(
+                text = "Tanciku+",
+                color = Color(0xFF94882B), // Warna emas atau warna yang menunjukkan premium
+                style = MaterialTheme.typography.labelSmall.copy(
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier
+                    .background(
+                        color = Color(0xFFFCF7D0), // Warna latar untuk badge
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+            )
 
 
         }
