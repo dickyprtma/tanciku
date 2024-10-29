34af571640c4b5e919b1a9f513f115d43d24db3e
dickyzuerst <dzuerst@gmail.com>
2024-10-29 23:57:08 +0700

    gunakan alokasi default

--- app/src/main/java/com/neonusa/tanciku/presentation/home/components/Transactionitem.kt
+++ app/src/main/java/com/neonusa/tanciku/presentation/home/components/Transactionitem.kt
@@ -2,6 +2,7 @@ package com.neonusa.tanciku.presentation.home.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +13,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -49,7 +51,11 @@ fun TransactionItem(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 24.dp, start = 16.dp, end = 16.dp)
-            .clickable { onClick?.invoke() },
+            .clickable(
+                onClick = { onClick?.invoke() },
+                indication = null, // Menghilangkan efek bayangan saat diklik
+                interactionSource = remember { MutableInteractionSource() }
+            ),
         verticalAlignment = Alignment.CenterVertically
     ) {
 
